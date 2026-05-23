ad21c0a68c9317ddba36f4bddd585af7a0d65566
Yousen <2105552902@qq.com>
2026-05-23 12:54:21 +0800

    Simplify CONFIG_PATH and CHECKPOINT_PATH variables
    Refactor configuration paths for clarity and simplicity.

--- test_VoD.sh
+++ test_VoD.sh
@@ -1,12 +1,10 @@
-CONFIG_PATH=./configs/r4det/vod-R4Det_det3d_2x4_12e.py
-CHECKPOINT_PATH=./work_dirs/vod-R4Det_det3d_2x4_12e/iter_2.pth #epoch_9.pth  #./checkpoints/epoch_3.pth#./xzy/epoch_3.pth #. # # #/output/vod/epoch_3.pth # #./checkpoints/FINAL-VoD.pth # #
-OUTPUT_NAME=pts_bbox #vod-R4Det
-PRED_RESULTS=./work_dirs/submissions/$OUTPUT_NAME  #./tools_det3d/view-of-delft-dataset/pred_results/$OUTPUT_NAME
+CONFIG_PATH=./configs/
+CHECKPOINT_PATH=./work_dirs/ 
+OUTPUT_NAME=pts_bbox
+PRED_RESULTS=./work_dirs/submissions/$OUTPUT_NAME 
 
 GPUS="8"
 PORT=$((RANDOM % 101 + 29600))
-#PORT=${PORT:-19521}
-#CUDA_VISIBLE_DEVICES="0,1,2,3" \
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 NATTEN_LOG_LEVEL="critical" python -m torch.distributed.launch \
     --nproc_per_node=$GPUS \
